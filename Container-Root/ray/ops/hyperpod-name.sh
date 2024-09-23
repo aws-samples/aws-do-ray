23f0057535fe5c453ad59f8ae6dc4a4bf60e5a6d
Mark Vinciguerra <mvincig@amazon.com>
2024-09-23 15:18:16 -0400

    hyperpod cluster name fix

--- Container-Root/ray/ops/hyperpod-name.sh
+++ Container-Root/ray/ops/hyperpod-name.sh
@@ -19,18 +19,27 @@ else
 fi
 
 if [ "$EKS_CLUSTER_NAME" == "" ]; then 
-	echo ""
-	echo "Could not determine EKS_CLUSTER_NAME"
-	echo ""
+	echo "" >&2
+	echo "Could not determine EKS_CLUSTER_NAME" >&2
+	echo "" >&2
 else
-	HP_CLUSTERS=$(aws sagemaker list-clusters --query ClusterSummaries[].ClusterName --output text)
+	HP_CLUSTERS=$(aws sagemaker list-clusters --region $AWS_REGION --query 'ClusterSummaries[].ClusterName' --output text)
 	for HP_CLUSTER in $HP_CLUSTERS; do 
-		echo $HP_CLUSTER; 
 		# TODO:
 		#Describe each hyperpod cluster and check the associated EKS cluster. 
 		#If you find a match, then echo the name of the identified hyperpod cluster
 		#Describe command (showing associated EKS cluster)
 		# aws sagemaker describe-cluster --cluster-name aws-do-hyperpod-eks-smhp --query Orchestrator.Eks.ClusterArn --output text
+		EKS_CLUSTER_ARN=$(aws sagemaker describe-cluster --cluster-name $HP_CLUSTER --region $AWS_REGION --query Orchestrator.Eks.ClusterArn --output text)
+		MAYBE_EKS_CLUSTER_NAME=$(echo $EKS_CLUSTER_ARN | awk -F'/' '{print $NF}')
+		# Check if the EKS cluster name matches the expected one
+		if [[ "$MAYBE_EKS_CLUSTER_NAME" == "$EKS_CLUSTER_NAME" ]]; then
+			echo "Match found! Hyperpod cluster: $HP_CLUSTER is associated with EKS cluster: $EKS_CLUSTER_NAME" >&2
+			export AWS_EKS_HYPERPOD_CLUSTER=$HP_CLUSTER
+			echo $HP_CLUSTER
+		else
+			echo "No match for cluster: $HP_CLUSTER"
+		fi
 	done
 fi
 
