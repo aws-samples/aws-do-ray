9103bbb3f56bbd8807d26135e42b265f9217eb1d
Mark Vinciguerra <mark12@bu.edu>
2024-09-24 14:36:53 +0000

    jobs-list, jobs-status, jobs-logs edits

--- Container-Root/ray/RayCluster/jobs/job-list.sh
+++ Container-Root/ray/RayCluster/jobs/job-list.sh
@@ -4,7 +4,7 @@ ray-expose.sh
 
 echo -e "JOBS:-------- \n"
 
-ray job list --address http://localhost:8265 
+ray job list --address http://localhost:8265 | sed -n "s/.*submission_id='\([^']*\)'.*entrypoint='\([^']*\)'.*/submission_id: \1, entrypoint: \2/p"
 
 echo -e "\nTo get the status of a job, please run './job-status.sh <submission_id>' where <submission_id> is listed for your jobs above."
 echo -e "\nTo get the logs of a job, please run './job-logs.sh <submission_id>' where <submission_id> is listed for your jobs above."
--- Container-Root/ray/RayCluster/jobs/job-status.sh
+++ Container-Root/ray/RayCluster/jobs/job-status.sh
@@ -4,10 +4,12 @@ ray-expose.sh
 
 # Check if the submission_id is passed as an argument
 if [ -z "$1" ]; then
+    echo ""
     echo "Error: No submission_id provided."
     echo "Usage: ./job-status.sh <submission_id>"
     echo "List of jobs to choose from:"
-    ray job list --address http://localhost:8265 
+    echo ""
+    ray job list --address http://localhost:8265 | sed -n "s/.*submission_id='\([^']*\)'.*entrypoint='\([^']*\)'.*/submission_id: \1, entrypoint: \2/p"
     echo -e "\n"
     exit 1
 fi
