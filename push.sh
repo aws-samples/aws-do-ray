f52b37978fa020b7ad4cc94e8e6e8cee67d1846c
Alex Iankoulski <iankouls@amazon.com>
2024-09-27 17:09:43 -0700

    add automation to image push

--- push.sh
+++ push.sh
@@ -2,6 +2,15 @@
 
 source .env
 
+# Create registry if needed
+REGISTRY_COUNT=$(aws ecr describe-repositories | grep \"${IMAGE}\" | wc -l)
+if [ "$REGISTRY_COUNT" == "0" ]; then
+	aws ecr create-repository --repository-name ${IMAGE}
+fi
+
+# Login to registry
+./login.sh
+
 CMD="docker image push ${REGISTRY}${IMAGE}${TAG}"
 
 if [ ! "$VERBOSE" == "false" ]; then echo -e "\n${CMD}\n"; fi
