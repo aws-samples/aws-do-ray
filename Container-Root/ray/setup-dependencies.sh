bc9b654e8c1baa8214526a45390e25715f0bc50c
flostahl-aws <162744951+flostahl-aws@users.noreply.github.com>
2024-12-19 17:13:31 +0100

    Update setup-dependencies.sh to kuberay v.1.2.0

--- Container-Root/ray/setup-dependencies.sh
+++ Container-Root/ray/setup-dependencies.sh
@@ -20,8 +20,8 @@ fi
 helm repo add kuberay https://ray-project.github.io/kuberay-helm/
 helm repo update
 
-#Install both CRDs and Kuberay operator v1.1.0
-helm install kuberay-operator kuberay/kuberay-operator --version 1.1.0 --namespace kuberay
+#Install both CRDs and Kuberay operator v1.2.0
+helm install kuberay-operator kuberay/kuberay-operator --version 1.2.0 --namespace kuberay
 
 # Kuberay operator pod will be deployed onto head pod
 kubectl get pods --namespace kuberay
